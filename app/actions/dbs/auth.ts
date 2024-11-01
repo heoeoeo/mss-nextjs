93664951ccb3dba6cfe26b9a4485c45bb5690d2c
jinhyeok <jinhyeok@jinhyeokui-MacBookAir.local>
2024-11-02 00:13:37 +0900

    fix: handleerror

--- app/actions/dbs/auth.ts
+++ app/actions/dbs/auth.ts
@@ -1,14 +1,27 @@
-import { SupabaseClient } from "@supabase/supabase-js"; // SupabaseClient 타입 임포트
+import { TypedSupabaseClient } from "utils/supabase/client";
 
-export async function getUserId(supabase: SupabaseClient) {
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
+/** 유저 인증 관련 함수 모아놓은 파일
+ * @returns { data, AuthErorr }
+ */
+const path = "app/actions/dbs/auth.ts";
 
+export async function getUser(supabase: TypedSupabaseClient) {
+  const { data } = await supabase.auth.getUser();
+
+  if (!data) {
+    console.error(path, "\nError in getUser: user가 없습니다.");
+    return null;
+  }
+
+  return data.user;
+}
+
+export async function getUserId(supabase: TypedSupabaseClient) {
+  const user = await getUser(supabase);
   return user.id;
 }
 
-export async function getUserRole(supabase: SupabaseClient) {
+export async function getUserRole(supabase: TypedSupabaseClient) {
   const {
     data: { user },
   } = await supabase.auth.getUser();
